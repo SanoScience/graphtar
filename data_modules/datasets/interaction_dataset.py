d6ae1905d0b41d69e6ffc690e2b3c5b7cd787bc0
janpr <jasiek.przybyszewski@gmail.com>
2022-06-06 17:08:53 +0200

    Add a generic interaction prediction Pytorch dataset.

---
+++ data_modules/datasets/interaction_dataset.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+from data_modules.datasets.transforms.merge import Merge
+
+
+class InteractionDataset(Dataset):
+    def __init__(self, csv_file_path: str, transform = None):
+        self.data_df = pd.read_csv(csv_file_path, index_col=0)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data_df)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        item = self.data_df.iloc[idx, [1, 3, 4]].to_numpy()
+        sample = {'mirna': item[:, 0], 'target': item[:, 1], 'label': item[:, 2]}
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+
+dataset = InteractionDataset("../../data/MirTarRAW.csv", transform=Merge('merged_sequences'))
+
+print(dataset[0:100])
