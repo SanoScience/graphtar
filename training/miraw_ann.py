496905ad0273827e5b222e0d68c654b341e72cd7
janpr <jasiek.przybyszewski@gmail.com>
2022-06-22 13:16:45 +0200

    Change directory structure.

--- training/miraw_ann.py
+++ training/miraw_ann.py
@@ -1,8 +1,8 @@
 import torch
 from pytorch_lightning import Trainer
 
-from data_modules.interaction_data_module import InteractionDataModule
-from lightning_modules.miraw_ann import MirawANN
+from data_modules.miraw_data_module import InteractionDataModule
+from lightning_modules.miraw.miraw_ann import MirawANN
 
 data_module = InteractionDataModule("../data/miRAW.csv", batch_size=128, train_val_ratio=(0.8, 0.2))
 x_key, y_key = data_module.get_batch_keys()
--- training/miraw_autoencoder.py
+++ training/miraw_autoencoder.py
@@ -1,8 +1,8 @@
 import torch
 from pytorch_lightning import Trainer
 
-from data_modules.interaction_data_module import InteractionDataModule
-from lightning_modules.miraw_autoencoder import MirawAutoencoderLM
+from data_modules.miraw_data_module import InteractionDataModule
+from lightning_modules.miraw.miraw_autoencoder import MirawAutoencoderLM
 
 data_module = InteractionDataModule("../data/miRAW.csv", batch_size=128, train_val_ratio=(0.8, 0.2))
 x_key, y_key = data_module.get_batch_keys()
