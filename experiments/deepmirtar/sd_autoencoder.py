85acbbbbe8e71db75277829c4cffa152b0757fcb
janpr <jasiek.przybyszewski@gmail.com>
2022-07-07 17:02:27 +0200

    Add neptune AI logger.

--- experiments/deepmirtar/sd_autoencoder.py
+++ experiments/deepmirtar/sd_autoencoder.py
@@ -1,12 +1,21 @@
 import sys
 
 import torch
+from dotenv import dotenv_values
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import NeptuneLogger
 
 from data_modules.interaction_data_module import InteractionDataModule
 from lightning_modules.deepmirtar.sd_autoencoder import SdAutoencoderLM
 from lightning_modules.models.deepmirtar.denoising_autoencoder import DenoisingAutoencoder
+
+config = dotenv_values("../../neptune_config.env")
+
+neptune_logger = NeptuneLogger(
+    project=config["NEPTUNE_PROJECT"],
+    api_token=config["NEPTUNE_API_TOKEN"],
+)
 
 config_path, data_split_seed, lr, batch_size, epochs_num = sys.argv
 config_name = config_path.split('/')[-1].split('.')[0]
@@ -30,6 +39,8 @@ for i, autoencoder in enumerate(autoencoders):
     else:
         module.append_module(autoencoder)
     trainer = Trainer(accelerator='gpu', max_epochs=epochs_num,
-                      callbacks=[EarlyStopping(monitor="val_loss", mode="min", patience=10)])
+                      callbacks=[EarlyStopping(monitor="val_loss", mode="min", patience=10)],
+                      logger=neptune_logger
+                      )
     trainer.fit(module, datamodule=data_module)
 torch.save(module.sda, "models/autoencoder_{}_{}_{}_{}.pt".format(config_name, batch_size, data_split_seed, lr))
