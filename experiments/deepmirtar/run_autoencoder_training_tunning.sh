c5efa53dbc587373e9e77b91c032cc49c461f5b3
janpr <jasiek.przybyszewski@gmail.com>
2022-07-08 11:52:14 +0200

    Fix autoencoder training script.

--- experiments/deepmirtar/run_autoencoder_training_tunning.sh
+++ experiments/deepmirtar/run_autoencoder_training_tunning.sh
@@ -3,7 +3,7 @@
 #SBATCH -J deepmirtar
 #SBATCH -N 1
 #SBATCH --tasks-per-node=3
-#SBATCH --time=01:00:00
+#SBATCH --time=00:15:00
 #SBATCH -A plgsano3
 #SBATCH -p plgrid-gpu-v100
 #SBATCH --gres=gpu:1
@@ -23,4 +23,4 @@ pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # config_path, data_split_seed, lr, batch_size, epochs_num
-python3 experiments/training/deepmirtar/sd_autoencoder.py "../../data_modules/configs/deepmirtar_config.json" 1234 0.001 128 10
+python3 experiments/deepmirtar/sd_autoencoder.py "../../data_modules/configs/deepmirtar_config.json" 1234 0.001 128 2
