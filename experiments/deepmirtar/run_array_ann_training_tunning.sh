513ae73dfd1a243a301ec5077c51b0708cf3186e
janprz <jasiek.przybyszewski@gmail.com>
2022-07-11 16:08:14 +0200

    Fix bug in the array tasks.

--- experiments/deepmirtar/run_array_ann_training_tunning.sh
+++ experiments/deepmirtar/run_array_ann_training_tunning.sh
@@ -9,7 +9,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --output="deepmirtar_ann.txt"
 #SBATCH --array=16,32,64,128,256,512
-#17072,613,2169,2697,7488,5258,2687,2775,8604,4435,4886,9997,3162,7189,8246,4506,8778,8566,4481,2389,9906,7142,9830,6295,4277,7053,8532,705,641,809
 
 module load plgrid/apps/cuda/11.3
 
@@ -25,4 +24,4 @@ pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # config_path, model, data_split_seed, lr, batch_size, epochs_num, model_path
-python3 experiments/deepmirtar/ann.py data_modules/configs/deepmirtar_config.json 1234 0.001 $SLURM_JOBID 1000 experiments/deepmirtar/models
+python3 experiments/deepmirtar/ann.py data_modules/configs/deepmirtar_config.json 1234 0.001 $SLURM_ARRAY_TASK_ID 1000 experiments/deepmirtar/models
--- experiments/deepmirtar/run_array_autoencoder_training_tunning.sh
+++ experiments/deepmirtar/run_array_autoencoder_training_tunning.sh
@@ -24,4 +24,4 @@ pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # config_path, data_split_seed, lr, batch_size, epochs_num, model_path
-python3 experiments/deepmirtar/sd_autoencoder.py data_modules/configs/deepmirtar_config.json 1234 0.001 $SLURM_JOBID 1000 experiments/deepmirtar/models
+python3 experiments/deepmirtar/sd_autoencoder.py data_modules/configs/deepmirtar_config.json 1234 0.001 $SLURM_ARRAY_TASK_ID 1000 experiments/deepmirtar/models
